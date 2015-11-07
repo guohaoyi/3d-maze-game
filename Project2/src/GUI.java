43a985bc1190adb896995a808ac812c159e84309
haoyiguo <haoyiguo@10.84.8.34>
2015-11-06 21:19:16 -0500

    Commit

--- Project2/src/GUI.java
+++ Project2/src/GUI.java
@@ -1,9 +1,28 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI {
+import javax.swing.JFrame;
+
+public class GUI extends JFrame implements ActionListener {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		GUI gui = new GUI();
 
+	}
+	
+	public GUI() {
+		super("Maze of Doom");
+		this.setSize(800, 800);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		this.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- Project2/src/Maze.java
+++ Project2/src/Maze.java
@@ -1,7 +1,39 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class Maze {
 	
 	private Room[][] rooms = new Room[50][50];
+	
+	public Maze() {
+		readMap();
+		readMonsters();
+		readTreasures();
+	}
+	
+	public void readMap() {
+		try {
+			FileReader fin = new FileReader("bin//map.txt");
+		} catch (FileNotFoundException e) {
+			System.out.println("Caught FileNotFoundException: " + e.getMessage());
+		}
+	}
+	
+	public void readMonsters() {
+		try {
+			FileReader fin = new FileReader("bin//monsters.txt");
+		} catch (FileNotFoundException e) {
+			System.out.println("Caught FileNotFoundException: " + e.getMessage());
+		}
+	}
+	
+	public void readTreasures() {
+		try {
+			FileReader fin = new FileReader("bin//treasures.txt");
+		} catch (FileNotFoundException e) {
+			System.out.println("Caught FileNotFoundException: " + e.getMessage());
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
