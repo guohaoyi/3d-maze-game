452ce91e9fa6b7b519fe484c00b4f14e3b5ab045
haoyiguo <haoyiguo@10.84.8.34>
2015-11-12 15:57:48 -0500

    Added buttons image

--- Project2/src/GUI.java
+++ Project2/src/GUI.java
@@ -1,9 +1,15 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -13,9 +19,22 @@ public class GUI extends JFrame {
 	private JPanel topPanel, bottomPanel, miniMapPanel;
 	private Maze maze = new Maze();
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		new GUI();
+		BufferedImage image = ImageIO.read(new File("bin/button.png"));
+
+	    int w = image.getWidth();
+	    int h = image.getHeight();
+
+	    int[] dataBuffInt = image.getRGB(0, 0, w, h, null, 0, w); 
+
+	    Color c = new Color(dataBuffInt[100]);
+
+	    System.out.println(c.getRed());   // = (dataBuffInt[100] >> 16) & 0xFF
+	    System.out.println(c.getGreen()); // = (dataBuffInt[100] >> 8)  & 0xFF
+	    System.out.println(c.getBlue());  // = (dataBuffInt[100] >> 0)  & 0xFF
+	    System.out.println(c.getAlpha()); 
 
 	}
 	
@@ -28,7 +47,7 @@ public class GUI extends JFrame {
 		
 		topPanel = new JPanel();
 		topPanel.setPreferredSize(new Dimension(800, 600));
-		topPanel.setBackground(Color.gray);
+		//topPanel.setBackground(Color.gray);
 		topPanel.setLayout(new BorderLayout());
 		topPanel.setBorder(BorderFactory.createLineBorder(Color.black));
 		this.add(topPanel, BorderLayout.NORTH);
@@ -42,14 +61,17 @@ public class GUI extends JFrame {
 		
 		bottomPanel = new JPanel();
 		bottomPanel.setPreferredSize(new Dimension(800, 200));
-		bottomPanel.setBackground(new Color(229, 204, 255));
+		bottomPanel.setBackground(new Color(200, 192, 230));
 		bottomPanel.setLayout(new BorderLayout());
 		bottomPanel.setBorder(BorderFactory.createLineBorder(Color.black));
 		this.add(bottomPanel, BorderLayout.SOUTH);
 		
+		Bottom bottom = new Bottom();
+		bottomPanel.add(bottom);
+		
 		miniMapPanel = new JPanel();
 		miniMapPanel.setPreferredSize(new Dimension(200, 200));
-		miniMapPanel.setBackground(Color.white);
+		miniMapPanel.setBackground(new Color(238, 238, 238));
 		miniMapPanel.setLayout(new BorderLayout());
 		bottomPanel.add(miniMapPanel, BorderLayout.EAST);
 
@@ -59,4 +81,17 @@ public class GUI extends JFrame {
 		this.setVisible(true);
 	}
 
+}
+
+class Bottom extends JComponent {
+	
+	public Bottom() {
+		this.setPreferredSize(new Dimension(800,200));
+	}
+	
+	public void paint(Graphics g) {
+		ImageIcon buttons = new ImageIcon("bin/buttons.png");
+		g.drawImage(buttons.getImage(), 324, 25, null);
+	}
+	
 }
--- Project2/src/GamePlay.java
+++ Project2/src/GamePlay.java
@@ -1,5 +1,9 @@
 
 public class GamePlay {
+	
+	public GamePlay() {
+		Player player = new Player();
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
